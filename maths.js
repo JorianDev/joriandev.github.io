3fe41942ced815d4e58e7d2799710f318722ab04
JorianDev <jfvdham@student.che.nl>
2025-10-28 18:55:38 +0100

    Slotpeilingen + countdown correction

--- maths.js
+++ maths.js
@@ -1,18 +1,23 @@
-var countDownTime = new Date("Oct 29, 2025 06:30:00").getTime(); //6.30 is 7.30 NL tijd
+//var countDownTime = new Date("Oct 29, 2025 06:30:00").getTime(); //6.30 is 7.30 NL tijd
+var countDownTime = new Date("2025-10-29T07:30:00+01:00").getTime();
 var interval = setInterval(function(){
 
     var now = new Date().getTime();
     var distance = countDownTime - now;
 
-    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor ((distance % (1000 * 60)) / 1000);
+    if (distance > 0) {
+        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor ((distance % (1000 * 60)) / 1000);
 
-    document.getElementById("countdown").innerHTML = "Tijd tot verkiezingen: " + days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+        document.getElementById("countdown").innerHTML = "Tijd tot verkiezingen: " + days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
 
-    if (distance < 0) {
-        clearInterval(interval);
+    } else if (distance > -48600000) {
         document.getElementById("countdown").innerHTML = "De tijd is om, de stembussen zijn geopend!"
+    } else {
+        clearInterval(interval);
+        document.getElementById("countdown").innerHTML = "U kunt niet meer stemmen!"
     }
+
 }, 1000);

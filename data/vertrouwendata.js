d551e81d5275bee42f3aff58a7fac450e3672706
JorianDev <jorianvdham@gmail.com>
2026-08-04 14:04:31 +0200

    Added linechart for politiek vertrouwen

---
+++ data/vertrouwendata.js
@@ -0,0 +1,116 @@
+const politiekVertrouwenData = {
+  "data": [
+    {
+      "date": "2012",
+      "gemeenteraad": null,
+      "europeseUnie": 39.2,
+      "ambtenaren": 44.3,
+      "tweedeKamer": 36.3,
+      "politici": null
+    },
+    {
+      "date": "2013",
+      "gemeenteraad": null,
+      "europeseUnie": 34.3,
+      "ambtenaren": 40.8,
+      "tweedeKamer": 31.5,
+      "politici": null
+    },
+    {
+      "date": "2014",
+      "gemeenteraad": null,
+      "europeseUnie": 36.4,
+      "ambtenaren": 41.9,
+      "tweedeKamer": 34.6,
+      "politici": null
+    },
+    {
+      "date": "2015",
+      "gemeenteraad": null,
+      "europeseUnie": 35.8,
+      "ambtenaren": 42.3,
+      "tweedeKamer": 34.4,
+      "politici": null
+    },
+    {
+      "date": "2016",
+      "gemeenteraad": null,
+      "europeseUnie": 36.0,
+      "ambtenaren": 42.6,
+      "tweedeKamer": 36.9,
+      "politici": 27.5
+    },
+    {
+      "date": "2017",
+      "gemeenteraad": null,
+      "europeseUnie": 43.1,
+      "ambtenaren": 45.5,
+      "tweedeKamer": 40.8,
+      "politici": 31.6
+    },
+    {
+      "date": "2018",
+      "gemeenteraad": null,
+      "europeseUnie": 45.2,
+      "ambtenaren": 46.6,
+      "tweedeKamer": 42.1,
+      "politici": 32.1
+    },
+    {
+      "date": "2019",
+      "gemeenteraad": null,
+      "europeseUnie": 45.7,
+      "ambtenaren": 46.3,
+      "tweedeKamer": 40.0,
+      "politici": 30.0
+    },
+    {
+      "date": "2020",
+      "gemeenteraad": 55.1,
+      "europeseUnie": 48.1,
+      "ambtenaren": 49.7,
+      "tweedeKamer": 53.2,
+      "politici": 39.7
+    },
+    {
+      "date": "2021",
+      "gemeenteraad": null,
+      "europeseUnie": 46.2,
+      "ambtenaren": 42.4,
+      "tweedeKamer": 42.4,
+      "politici": 33.3
+    },
+    {
+      "date": "2022",
+      "gemeenteraad": 50.7,
+      "europeseUnie": 48.7,
+      "ambtenaren": 42.5,
+      "tweedeKamer": 30.4,
+      "politici": 23.8
+    },
+    {
+      "date": "2023",
+      "gemeenteraad": 51.5,
+      "europeseUnie": 47.1,
+      "ambtenaren": 44.0,
+      "tweedeKamer": 29.0,
+      "politici": 23.8
+    },
+    {
+      "date": "2024",
+      "gemeenteraad": 53.4,
+      "europeseUnie": 49.7,
+      "ambtenaren": 47.4,
+      "tweedeKamer": 31.3,
+      "politici": 25.1
+    },
+    {
+      "date": "2025",
+      "gemeenteraad": 54.5,
+      "europeseUnie": 51.5,
+      "ambtenaren": 47.1,
+      "tweedeKamer": 24.6,
+      "politici": 21.2
+    }
+  ]
+};
